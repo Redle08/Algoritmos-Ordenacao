f9a5eb3b67fe184ab57c2f8f0a201dc99862470f
Redle08 <elder08faber@gmail.com>
2025-06-01 17:14:23 -0300

    Bucketsort.py

--- Bucketsort.py
+++ Bucketsort.py
@@ -2,21 +2,22 @@
 # Vantagens: Muito eficiente para dados uniformemente distribuídos.
 # Desvantagens: Não é bom para dados não uniformes ou com muitos valores repetidos.
 
+lista = [5, 2, 4, 3, 1]
 
 def bucket_sort(lista):
     if not lista:
-      return lista
+        return lista
 
     num_buckets = len(lista)
     buckets = [[] for _ in range(num_buckets)]
 
     max_val = max(lista)
     for num in lista:
-        index = min(int(num * num_buckets / (max_val+1)), num_buckets-1)
+        index = min(int(num * num_buckets / (max_val + 1)), num_buckets - 1)
         buckets[index].append(num)
 
     for bucket in buckets:
-      bucket.sort()
+        bucket.sort()
 
     sorted_lista = []
     for bucket in buckets:
@@ -24,3 +25,6 @@ def bucket_sort(lista):
 
     return sorted_lista
 
+
+lista_ordenada = bucket_sort(lista)
+print(lista_ordenada)
