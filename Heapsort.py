56c5e96efc98e1c5ada77233e05175454f51391c
Redle08 <elder08faber@gmail.com>
2025-06-01 17:15:51 -0300

    Heapsort.py

--- Heapsort.py
+++ Heapsort.py
@@ -1,8 +1,12 @@
 # 6. Heap Sort
 # Vantagens: Complexidade O(n log n), não usa memória extra.
 # Desvantagens: Não é estável, implementação mais complexa.
+lista = [5,2,4,3,1]
 import heapq
 
 def heap_sort(lista):
     heapq.heapify(lista)
     return [heapq.heappop(lista) for _ in range(len(lista))]
+
+lista_ordenada = heap_sort(lista)
+print(lista_ordenada)
