9407164e73bfa76c4f5c1d8f03f717af21932070
Redle08 <elder08faber@gmail.com>
2025-06-01 17:18:30 -0300

    Selectionsort.py

--- Selectionsort.py
+++ Selectionsort.py
@@ -1,7 +1,7 @@
 # 1. Selection Sort (Ordenação por Seleção)
 # Vantagens: Simples de implementar, não requer memória adicional.
 # Desvantagens: Ineficiente para listas grandes (complexidade O(n²)).
-
+lista = [5,2,4,3,1]
 def selection_sort(lista):
     n = len(lista)
     for i in range(n):
@@ -11,5 +11,6 @@ def selection_sort(lista):
                 indice_minimo = j
         lista[i], lista[indice_minimo] = lista[indice_minimo], lista[i]
     return lista
-
+lista_ordenada = selection_sort(lista)
+print(lista_ordenada)
 
