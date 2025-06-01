ea1f10a94e20b421ff638eadc530f7671d3a7f98
Redle08 <elder08faber@gmail.com>
2025-06-01 17:17:40 -0300

    Quicksort.py

--- Quicksort.py
+++ Quicksort.py
@@ -1,6 +1,7 @@
 # 5. Quick Sort (Ordenação Rápida)
 # Vantagens: Muito eficiente na prática (O(n log n) em média).
 # Desvantagens: Pode ter pior caso O(n^2), não é estável.
+lista = [5,2,4,3,1]
 def quick_sort(lista):
     if len(lista) <= 1:
         return lista
@@ -8,3 +9,5 @@ def quick_sort(lista):
     menores = [x for x in lista[1:] if x <= pivo]
     maiores = [x for x in lista[1:] if x > pivo]
     return quick_sort(menores) + [pivo] + quick_sort(maiores)
+lista_ordenada = quick_sort(lista)
+print(lista_ordenada)
